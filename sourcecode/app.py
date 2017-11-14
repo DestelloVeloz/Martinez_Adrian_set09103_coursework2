d7cd1b97ae5b93545f32db887621de461969af7a
DestelloVeloz <40174677@live.napier.ac.uk>
2017-11-14 22:32:58 +0000

    backup before adding new selection menu

--- sourcecode/app.py
+++ sourcecode/app.py
@@ -4,7 +4,7 @@ app.secret_key='\xd9\xa8\xf5\xafm\xec\xa2J\x11`\x8fH\xbeO\xeb\x86\x05\xaf"\xfc\x
 
 @app.route('/')
 def home():
-	return render_template('BT_test.html')
+	return render_template('home.html')
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=9116 , debug=True)
